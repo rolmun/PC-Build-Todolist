c7b4fde9dca0128d80758d4b4b899c28be925303
Rolando Munoz <rolandomunoz55@gmail.com>
2024-03-23 19:07:41 -0700

    Console log input on inspect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+//App.js
 
 import './App.css';
+import { TodoWrapper } from './components/TodoWrapper'; //need to import component from file path
 
 function App() {
   return (
     <div className="App">
+      <TodoWrapper />
 
     </div>
   );
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TodoForm = () => {
+    const [value, setValue] = useState("") //empy sting intitially
   return (
-    <div>TodoForm</div>
+    <form className='TodoForm'>
+        <input type ="text" className='todo-input' 
+        placeholder='What is your task?'
+        onchange = {(e) => console.log(e.target.value)} /> 
+        <button type ='submit' className='todo-btn'> Add Task</button>
+        </form>
   )
 }
